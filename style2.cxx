9ad43baffbcb9d84a7a3cc4beab242fdeae3ef62
Minke Houthuesen <minkehouthuesen@hotmail.com>
2014-03-30 16:00:08 +0200

    nieuwe index probeersel

---
+++ style2.cxx
@@ -0,0 +1,23 @@
+body {
+	margin-left:200px;
+	font-family: Helvetica, Arial;
+	background:fixed url("plaatje_boom") no-repeat center left;
+	font-size: 14px;
+	color: #444444;
+}
+
+.container
+{
+	text-align:center;
+}
+
+.center_div
+{
+	border:1px solid black;
+	margin-left:auto;
+	margin-right:auto;
+	width:90%;
+	background-color:#FFFFFF;
+	text-align:left;
+	padding:8px;
+}
